9e570d380d119d9e809bbf531c9c0cc5f692f400
Deep Dev <deepdevpro@gmail.com>
2025-05-24 19:27:01 -0300

    chg: removendo otimização das músicas

--- app/__init__.py
+++ app/__init__.py
@@ -158,7 +158,7 @@ def admin_dashboard():
     musicas = Track.query.all()
 
     # Calcula a duração total da playlist
-    total_segundos = sum(m.duracao_segundos for m in musicas)
+    total_segundos = sum(m.duracao_segundos or 0 for m in musicas)
     total_formatado = str(timedelta(seconds=total_segundos))
 
     return render_template("admin-dashboard.html",
@@ -291,102 +291,6 @@ ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 def extensao_permitida(nome_arquivo):
     return "." in nome_arquivo and \
             nome_arquivo.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# @app.route("/upload-imagens", methods=["POST"])
-# def upload_imagens():
-#     logger.info("[UPLOAD]  🚀 Iniciando rota /upload-imagens")
-
-#     if "imagens" not in request.files:
-#         logger.info("[UPLOAD][ERRO] Nenhum campo 'imagens' encontrado em request.files")
-#         return {"erro": "Nenhum arquivo encontrado"}, 400
-    
-#     arquivos = request.files.getlist("imagens")
-#     logger.info(f"[UPLOAD] Total de arquivos recebidos: {len(arquivos)}")
-
-#     salvos = []
-
-#     for i, arquivo in enumerate(arquivos):
-#         logger.info(f"[UPLOAD] >> Processando arquivo {i + 1}/{len(arquivos)}")
-#         logger.info(f"[UPLOAD] Nome original: {arquivo.filename}")
-        
-#         if not arquivo:
-#             logger.info(f"[UPLOAD][ERRO] Arquivo {i + 1} está vazio ou inválido")
-#             continue
-
-#         if not extensao_permitida(arquivo.filename):
-#             logger.info(f"[UPLOAD][ERRO] Extensão nnao permitida: {arquivo.filename}")
-#             continue
-
-#         try:
-#             nome_seguro = secure_filename(arquivo.filename)
-#             logger.info(f"[UPLOAD] Nome seguro gerado: {nome_seguro}")
-
-#             imagem = Image.open(arquivo)
-#             imagem = imagem.convert("RGB")
-
-#             # Redimensiona imagem original para no máx. 1600x1600
-#             imagem.thumbnail((1600, 1600))
-#             logger.info("[UPLOAD] Imagem convertida e redimensionada com sucesso")
-
-#             buffer = BytesIO()
-#             imagem.save(buffer, format="JPEG", quality=65, optimize=True)
-#             buffer.seek(0)
-#             logger.info(f"[UPLOAD] Buffer principal pronto: {buffer.getbuffer().nbytes} bytes")
-
-#             # Envia imagem otimizada
-#             logger.info("[UPLOAD] Enviando imagem otimizada para o S3...")
-#             upload_arquivo_s3(buffer, nome_seguro, pasta="static/img/galeria", content_type="image/jpeg")
-#             logger.info(f"[UPLOAD] Upload da imagem principal concluída: {nome_seguro}")
-
-#             # Miniaturas
-#             thumb = imagem.copy()
-#             thumb.thumbnail((300, 300))
-#             buffer_thumb = BytesIO()
-#             thumb.save(buffer_thumb, format="JPEG", quality=50, optimize=True)
-#             buffer_thumb.seek(0)
-#             logger.info(f"[UPLOAD] Buffer da miniatura pronto: {buffer_thumb.getbuffer().nbytes} bytes")
-
-#             nome_thumb = f"thumb_{nome_seguro}"
-#             logger.info(f"[UPLOAD] Nome da miniatura: {nome_thumb}")
-
-#             logger.info("[UPLOAD] Enviando miniatura para o S3...")
-#             upload_arquivo_s3(buffer_thumb, nome_thumb, pasta="static/img/galeria", content_type="image/jpeg")
-#             logger.info(f"[UPLOAD] Upload da miniatura concluído: {nome_thumb}")
-
-#             salvos.append(nome_thumb)
-        
-#         except Exception as e:
-#             logger.info(f"[UPLOAD][ERRO] Falha ao processar/enviar '{arquivo.filename}': {str(e)}")
-#             continue
-    
-#     logger.info(f"[UPLOAD] ✅ Upload finalizado. Total de miniaturas salvas: {len(salvos)}")
-    
-#     if salvos:
-#         return redirect(url_for("admin_dashboard", aba="imagens"))
-#     else:
-#         return {"erro": "Nenhum arquivo válido"}, 400
-
-# @app.route("/definir-fundo", methods=["POST"])
-# def definir_fundo():
-#     imagem = request.form.get("imagem")
-
-#     if not imagem:
-#         return{"erro": "Imagem não informada"}, 400
-    
-#     # caminho_config = "config.json"
-#     caminho_config = os.path.join(os.path.dirname(__file__), "..", "config.json")
-#     caminho_config = os.path.abspath(caminho_config)
-
-
-#     with open(caminho_config, "r") as f:
-#         config = json.load(f)
-
-#     config["background_image"] = imagem
-
-#     with open(caminho_config, "w") as f:
-#         json.dump(config, f, indent=2)
-    
-#     return redirect(url_for("admin_dashboard", aba="imagens"))
 
 def log_mem(tag=""):
     process = psutil.Process(os.getpid())
