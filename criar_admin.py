ac4ce9e286660707da1ce342cbab4a27d38f17e1
Deep Dev <deepdevpro@gmail.com>
2025-05-28 16:00:32 -0300

    add: cadastrando user leo

--- criar_admin.py
+++ criar_admin.py
@@ -3,10 +3,9 @@ from app import db
 from app.models import User
 
 with app.app_context():
-    admin = User(nome="leo", email="leoogura@hotmail.com")
+    admin = User(email="leoogura@hotmail.com")
     admin.set_senha("senhaSegura1234")
     db.session.add(admin)
     db.session.commit()
 
     print("Admin criado com sucesso!")
-
